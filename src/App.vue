2afe77636ffef9ecba5705e4d6b1d59883bcb5a5
minitecnico <minitecnico@gmail.com>
2026-02-12 01:25:41 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -173,8 +173,8 @@ const exportarCSV = async () => {
             <p><strong>Login:</strong> {{ form.cpf }} | <strong>Senha:</strong> 123456</p>
           </div>
           <div class="btn-group">
-            <button @click="gerarPDF" class="btn-pdf">📥 BAIXAR PDF</button>
-            <button @click="exportarCSV" class="btn-csv">📊 PLANILHA</button>
+            <button @click="gerarPDF" class="btn-pdf">disabled </button>
+            <button @click="exportarCSV" class="btn-csv">disabled </button>
             <button @click="reset" class="btn-new">NOVO CADASTRO</button>
           </div>
         </div>
